e26c88c1854e55a1958b752e06841c5f4c4178c5
JulieLB <julie.leborgne78@hotmail.fr>
2020-05-19 12:21:39 +0200

    Adding interpretation tools.

--- R/MCA.R
+++ R/MCA.R
@@ -24,9 +24,11 @@ MCA <- function(Y, n = 5) {
   )
   )
 
+
+  #individus
   F <- res$P %*% diag(sqrt(res$D))[,1:n] #diag(as.numeric(res$r)^(-1/2)) %*% res$P %*% diag(sqrt(res$D))[,1:n]
   F2 <- F^2
-  contrib <- (F2) %*% diag(1/colSums(F2)) * 100
+  contrib <- 100/sum(X) * diag(rowSums(X)) %*% F2 %*% diag(1/res$D[1:n]) #(F2) %*% diag(1/colSums(F2)) * 100 #
   cos2 <- t(t(F2)%*%diag(1/rowSums(F2)))
 
   col <- paste("dim", seq(from = 1, by = 1, length = n))
@@ -34,22 +36,23 @@ MCA <- function(Y, n = 5) {
   rownames(F) <- rownames(contrib) <- rownames (cos2) <- rownames (Y)
   ind <- list(coord = F, contrib = contrib, cos2 = cos2)
 
-
+  #categories
   G <- res$Q %*% diag(sqrt(res$D))[,1:n] #diag(as.numeric(res$c)^(-1/2)) %*% res$Q %*% diag(sqrt(res$D))[,1:n]
-  contrib <- (G^2) %*% diag(1/colSums(G^2)) * 100 #ameliorer : mettre G^2 et F^2 dans un objet
-  cos2 <- t(t(G^2)%*%diag(1/rowSums(G^2)))
+  G2 <- G^2
+  contrib <- 1/sum(X) * diag(colSums(X)) %*% G2 #contribution absolue
+  cos2 <- t(t(G2)%*%diag(1/rowSums(G2)))
 
   partition <- partition_variables(Y)
 
   eta2 <- c()
   for (j in 1:n) {
-    eta2 <- cbind(eta2, sapply(1:ncol(Y), function(i) {sum((G[partition[[i]],j])^2)/sum(G[,j]^2)}))
+    eta2 <- cbind(eta2, sapply(1:ncol(Y), function(i) {sum(contrib[partition[[i]],j])}))
   }
 
   colnames(G) <- colnames(contrib) <- colnames (cos2) <- colnames(eta2) <- col
   rownames(G) <- rownames(contrib) <- rownames (cos2) <- colnames (X)
   rownames(eta2) <- colnames(Y)
-  var <- list(coord = G, contrib = contrib, cos2 = cos2, eta2 = eta2)
+  var <- list(coord = G, contrib = 100 * contrib %*% diag(1/res$D[1:n]), cos2 = cos2, eta2 = 10 * eta2)
 
 
   return(list (gsvd = res,
--- R/SMCA.R
+++ R/SMCA.R
@@ -32,31 +32,36 @@ SMCA <- function(Y, c1, c2, n = 5, meth ='cgsvd', init = "rand", v.partition = F
                              cumulatedPercentageOfVariance = cumsum(sapply (1:length(res$D), function(j){res$D[j]/sum(res$D)*100}))
                              )
                        )
+
+
+  #individus
   F <- res$P %*% diag(sqrt(res$D))[,1:n] #diag(as.numeric(res$r)^(-1/2)) %*% res$P %*% diag(sqrt(res$D))[,1:n]
-  contrib <- (F^2) %*% diag(1/colSums(F^2))*100
-  cos2 <- t(t(F^2)%*%diag(1/rowSums(F^2)))
+  F2 <- F^2
+  contrib <- 100/sum(X) * diag(rowSums(X)) %*% F2 %*% diag(1/res$D[1:n]) #(F2) %*% diag(1/colSums(F2)) * 100 #
+  cos2 <- t(t(F2)%*%diag(1/rowSums(F2)))
 
   col <- paste("dim", seq(from = 1, by = 1, length = n))
   colnames(F) <- colnames(contrib) <- colnames (cos2) <- col
   rownames(F) <- rownames(contrib) <- rownames (cos2) <- rownames (Y)
   ind <- list(coord = F, contrib = contrib, cos2 = cos2)
 
-
+  #categories
   G <- res$Q %*% diag(sqrt(res$D))[,1:n] #diag(as.numeric(res$c)^(-1/2)) %*% res$Q %*% diag(sqrt(res$D))[,1:n]
-  contrib <- (G^2) %*% diag(1/colSums(G^2))*100
-  cos2 <- t(t(G^2)%*%diag(1/rowSums(G^2)))
+  G2 <- G^2
+  contrib <- 1/sum(X) * diag(colSums(X)) %*% G2 #contribution absolue
+  cos2 <- t(t(G2)%*%diag(1/rowSums(G2)))
 
   partition <- partition_variables(Y)
+
   eta2 <- c()
   for (j in 1:n) {
-    eta2 <- cbind(eta2, sapply(1:ncol(Y), function(i) {sum((G[partition[[i]],j])^2)/sum(G[,j]^2)}))
+    eta2 <- cbind(eta2, sapply(1:ncol(Y), function(i) {sum(contrib[partition[[i]],j])}))
   }
 
   colnames(G) <- colnames(contrib) <- colnames (cos2) <- colnames(eta2) <- col
   rownames(G) <- rownames(contrib) <- rownames (cos2) <- colnames (X)
   rownames(eta2) <- colnames(Y)
-  var <- list(coord = G, contrib = contrib, cos2 = cos2, eta2 = eta2)
-
+  var <- list(coord = G, contrib = 100 * contrib %*% diag(1/res$D[1:n]), cos2 = cos2, eta2 = 10 * eta2)
 
 
   return(list (cgsvd = res,
